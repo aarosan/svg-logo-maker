80320e1b72db6eb5a7be3de91acf0be3837773bb
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-01-17 10:08:56 -0600

    initial svg write file shape

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import fs from 'fs';
 import inquirer from 'inquirer';
 
+//import createLogo() from shapes.js
+
+import { createLogo } from './lib/shapes.js'
+
 const questions = [
     {
         type: 'input',
@@ -18,22 +22,7 @@ const questions = [
         type: 'input',
         name: 'textColor',
         message: 'Please enter a color for the text. It can be a color keyword or a Hexidecimal number',
-        validate: function(input) {
-
-            //Got from chatGPT
-            const hexColorRegex = /^#([0-9A-Fa-f]{3}){1,2}\b$/;  
-            const colorKeywords = [
-                'black', 'white', 'red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'purple', 'orange', 'pink', 'brown', 'gray / grey', 'silver', 'gold', 'aqua', 'indigo', 'violet', 'turquoise', 'teal', 'maroon', 'olive', 'lavender', 'coral', 'peach', 'beige', 'ivory', 'mint', 'khaki', 'plum', 'navy', 'tan', 'fuchsia', 'crimson', 'ruby', 'emerald', 'sapphire', 'amethyst', 'topaz', 'garnet', 'amber', 'onyx', 'opal', 'pearl', 'jade', 'granite', 'charcoal', 'slate', 'brick', 'terracotta', 'azure', 'sepia', 'mauve', 'chartreuse', 'moss', 'lemon', 'tangerine', 'caramel', 'strawberry', 'lilac', 'cobalt', 'periwinkle', 'pewter', 'sand', 'hazel', 'sienna', 'copper', 'mahogany', 'denim', 'biscuit', 'ash', 'chestnut', 'cornflower', 'mustard', 'olive', 'pistachio', 'raspberry', 'strawberry', 'lavender', 'orchid', 'pumpkin', 'sky blue', 'eggplant', 'burgundy', 'taupe', 'vermilion', 'lemonade', 'raspberry', 'snow', 'firebrick', 'timberwolf', 'thistle', 'orchid', 'shamrock', 'raspberry', 'mauve', 'heliotrope', 'palatinate blue', 'cerulean', 'shocking pink', 'saffron', 'razzmatazz', 'tiffany blue', 'harlequin', 'bondi blue', 'electric crimson', 'vivid tangerine', 'rufous', 'fulvous', 'wenge', 'smaragdine', 'han purple', 'mountbatten pink', 'old lavender', 'old mauve', 'old rose', 'old gold', 'old silver'
-            ]
-
-            if (colorKeywords.includes(input.toLowerCase()) || hexColorRegex.test(input)) {
-                return true;
-                //Test to see if the color is accepted and what was accepted
-                console.log(answers.textColor);
-            }
-            
-            return 'Please enter a valid color keyword or a valid hex color.';
-        }
+        validate: validateColor,
     },
     {
         type: 'list',
@@ -45,45 +34,48 @@ const questions = [
         type: 'input',
         name: 'shapeColor',
         message: 'Please enter a color for the shape. It can be a color keyword or a Hexidecimal number',
-        validate: function(input) {
-
-            //Got from chatGPT
-            const hexColorRegex = /^#([0-9A-Fa-f]{3}){1,2}\b$/;  
-            const colorKeywords = [
-                'black', 'white', 'red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'purple', 'orange', 'pink', 'brown', 'gray / grey', 'silver', 'gold', 'aqua', 'indigo', 'violet', 'turquoise', 'teal', 'maroon', 'olive', 'lavender', 'coral', 'peach', 'beige', 'ivory', 'mint', 'khaki', 'plum', 'navy', 'tan', 'fuchsia', 'crimson', 'ruby', 'emerald', 'sapphire', 'amethyst', 'topaz', 'garnet', 'amber', 'onyx', 'opal', 'pearl', 'jade', 'granite', 'charcoal', 'slate', 'brick', 'terracotta', 'azure', 'sepia', 'mauve', 'chartreuse', 'moss', 'lemon', 'tangerine', 'caramel', 'strawberry', 'lilac', 'cobalt', 'periwinkle', 'pewter', 'sand', 'hazel', 'sienna', 'copper', 'mahogany', 'denim', 'biscuit', 'ash', 'chestnut', 'cornflower', 'mustard', 'olive', 'pistachio', 'raspberry', 'strawberry', 'lavender', 'orchid', 'pumpkin', 'sky blue', 'eggplant', 'burgundy', 'taupe', 'vermilion', 'lemonade', 'raspberry', 'snow', 'firebrick', 'timberwolf', 'thistle', 'orchid', 'shamrock', 'raspberry', 'mauve', 'heliotrope', 'palatinate blue', 'cerulean', 'shocking pink', 'saffron', 'razzmatazz', 'tiffany blue', 'harlequin', 'bondi blue', 'electric crimson', 'vivid tangerine', 'rufous', 'fulvous', 'wenge', 'smaragdine', 'han purple', 'mountbatten pink', 'old lavender', 'old mauve', 'old rose', 'old gold', 'old silver'
-            ]
-
-            if (colorKeywords.includes(input.toLowerCase()) || hexColorRegex.test(input)) {
-                return true;
-                //Test to see if the color is accepted and what was accepted
-                console.log(answers.textColor);
-            }
-            
-            return 'Please enter a valid color keyword or a valid hex color.';
-        }
+        validate: validateColor,
     }
 ]
+
+
+function validateColor(input) {
+
+    //Got from chatGPT
+    const hexColorRegex = /^#([0-9A-Fa-f]{3}){1,2}\b$/;  
+    const colorKeywords = [
+        'black', 'white', 'red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'purple', 'orange', 'pink', 'brown', 'gray / grey', 'silver', 'gold', 'aqua', 'indigo', 'violet', 'turquoise', 'teal', 'maroon', 'olive', 'lavender', 'coral', 'peach', 'beige', 'ivory', 'mint', 'khaki', 'plum', 'navy', 'tan', 'fuchsia', 'crimson', 'ruby', 'emerald', 'sapphire', 'amethyst', 'topaz', 'garnet', 'amber', 'onyx', 'opal', 'pearl', 'jade', 'granite', 'charcoal', 'slate', 'brick', 'terracotta', 'azure', 'sepia', 'mauve', 'chartreuse', 'moss', 'lemon', 'tangerine', 'caramel', 'strawberry', 'lilac', 'cobalt', 'periwinkle', 'pewter', 'sand', 'hazel', 'sienna', 'copper', 'mahogany', 'denim', 'biscuit', 'ash', 'chestnut', 'cornflower', 'mustard', 'olive', 'pistachio', 'raspberry', 'strawberry', 'lavender', 'orchid', 'pumpkin', 'sky blue', 'eggplant', 'burgundy', 'taupe', 'vermilion', 'lemonade', 'raspberry', 'snow', 'firebrick', 'timberwolf', 'thistle', 'orchid', 'shamrock', 'raspberry', 'mauve', 'heliotrope', 'palatinate blue', 'cerulean', 'shocking pink', 'saffron', 'razzmatazz', 'tiffany blue', 'harlequin', 'bondi blue', 'electric crimson', 'vivid tangerine', 'rufous', 'fulvous', 'wenge', 'smaragdine', 'han purple', 'mountbatten pink', 'old lavender', 'old mauve', 'old rose', 'old gold', 'old silver'
+    ]
+
+    if (colorKeywords.includes(input.toLowerCase()) || hexColorRegex.test(input)) {
+        return true;
+        //Test to see if the color is accepted and what was accepted
+        console.log(answers.textColor);
+    }
+    
+    return 'Please enter a valid color keyword or a valid hex color.';
+}
+
 
 // Function to create the svg file
 
 // UNCOMMENT ONCE YOU FINISH SHAPES.JS
-// function writeToFile(fileName, data) {
-//     fs.writeFile(fileName, shapesFunction(data), (err) => {
-//         if(err) {
-//             console.error('Error creating the file:', err);
-//             return;
-//         }
-//         console.log('Generated logo.svg');
-//     })
-// };
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, createLogo(data), (err) => {
+        if(err) {
+            console.error('Error creating the file:', err);
+            return;
+        }
+        console.log('Generated logo.svg');
+    })
+};
 
 function init() {
     inquirer.prompt(questions)
     .then((answers) => {
         console.log(answers);
         const filename = 'file.svg';
-        // Uncomment once ready to test
-        // writeToFile(filename, answers);
+        writeToFile(filename, answers);
     })
 }
 
