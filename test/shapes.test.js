4d79202db743bef090d5216a584b59cbdff0f7d4
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-01-18 12:42:48 -0600

    added shapeColor

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -2,9 +2,22 @@ const { createShape } = require('../lib/shapes.js');
 
 describe("Shape test", () => {
     test("test for circle", () => {
-        const testData = { shape: 'circle' };
+        const testData = { shape: 'circle', shapeColor: 'red' };
         expect(createShape(testData)).toEqual(
-            '<circle cx="25" cy="75" r="20"/>'
+            `<circle cx="25" cy="75" r="20" fill="${testData.shapeColor}"/>`
+        );
+    });
+    test("test for triangle", () => {
+        const testData = { shape: 'triangle', shapeColor: '#0000FF' };
+        expect(createShape(testData)).toEqual(
+            `<polygon points="50 15, 100 100, 0, 100" fill="${testData.shapeColor}"/>`
+        );
+    });
+    test("test for square", () => {
+        const testData = { shape: 'square', shapeColor: 'yellow' };
+        expect(createShape(testData)).toEqual(
+            `<rect x="10" y="10" width="30" height="30" fill="${testData.shapeColor}"/>`
         );
     });
 });
+
